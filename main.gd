1027180351dc23dec2227685e1f9484a8e6b4b02
Aaron <aaron@yieldsystems.tech>
2024-01-27 17:39:09 +0200

    Better wave implementation

--- main.gd
+++ main.gd
@@ -5,21 +5,19 @@ extends Node2D
 #Format:
 #[Duration, How often do we spawn these enemies, [enemies]
 var enemy_table = [
-	[0, 1, []],
 	[10, 1, [EnemyScene, EnemyScene]],
 	[10, 1, [EnemyScene, EnemyScene]],
 ]
 var time_elapsed = 0.0
-
+var next_wave_at = 10.0
+var wave = 0
 
 func spawn_enemies():
-	var index = 0
-	var cutoff = 0.0
+	if next_wave_at <= time_elapsed && wave < enemy_table.size() - 1: 
+		next_wave_at += enemy_table[wave][0]
+		wave += 1
+		$EnemySpawnCooldown.wait_time = enemy_table[wave][1]
 
-	while cutoff < time_elapsed && index < enemy_table.size() - 1:
-		cutoff += enemy_table[index][0]
-		index += 1
-		$EnemySpawnCooldown.wait_time = enemy_table[index][1]
 
 	var viewport = get_viewport_rect()
 	var viewport_size = viewport.size
@@ -38,7 +36,7 @@ func spawn_enemies():
 
 	if $EnemySpawnCooldown.is_stopped():
 		var pos = enemy_spawn_position_table.pick_random()
-		for enemy in enemy_table[index][2]:
+		for enemy in enemy_table[wave][2]:
 			var new_enemy = enemy.instantiate()
 			new_enemy.position = pos
 			add_child(new_enemy)
