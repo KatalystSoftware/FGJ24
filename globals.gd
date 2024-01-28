6bee6883ac445bb8d787289b237ef8d28df387df
Mikael Siidorow <mikael.siidorow@aalto.fi>
2024-01-28 10:09:57 +0200

    refactor upgrade logic to globals

--- globals.gd
+++ globals.gd
@@ -1,3 +1,44 @@
 extends Node
 
+enum UpgradeOption {
+	INCREASE_HEALTH,
+	INCREASE_MOVEMENT_SPEED,
+	INCREASE_AMOUNT,
+	INCREASE_DAMAGE,
+	INCREASE_EXPERIENCE,
+	REDUCE_COOLDOWN,
+	INCREASE_SHOT_SPEED,
+	INCREASE_SHOT_LIFETIME,
+	REDUCE_SPREAD
+}
+
 var is_dying = false
+
+
+func get_random_upgrades():
+	var shuffled_options = UpgradeOption.keys()
+	shuffled_options.shuffle()
+	var options = shuffled_options.slice(0, 4)
+	return options
+
+
+func upgrade_stat(option: UpgradeOption) -> void:
+	match option:
+		UpgradeOption.INCREASE_HEALTH:
+			PlayerStats.max_health += PlayerStats.BASE_MAX_HEALTH * 0.1
+		UpgradeOption.INCREASE_MOVEMENT_SPEED:
+			PlayerStats.movement_speed += PlayerStats.BASE_MOVEMENT_SPEED * 0.1
+		UpgradeOption.INCREASE_EXPERIENCE:
+			PlayerStats.experience_multiplier += PlayerStats.BASE_EXPERIENCE_MULTIPLIER * 0.1
+		UpgradeOption.INCREASE_AMOUNT:
+			PlayerStats.shot_amount += 1
+		UpgradeOption.INCREASE_DAMAGE:
+			PlayerStats.shot_damage += 1
+		UpgradeOption.REDUCE_COOLDOWN:
+			PlayerStats.shot_cooldown -= PlayerStats.BASE_SHOT_COOLDOWN * 0.1
+		UpgradeOption.INCREASE_SHOT_SPEED:
+			PlayerStats.shot_speed += PlayerStats.BASE_SHOT_SPEED * 0.1
+		UpgradeOption.INCREASE_SHOT_LIFETIME:
+			PlayerStats.shot_lifetime += PlayerStats.BASE_SHOT_LIFETIME * 0.1
+		UpgradeOption.REDUCE_SPREAD:
+			PlayerStats.shot_spread -= PlayerStats.BASE_SHOT_SPREAD * 0.05
--- ui/upgrade_options.gd
+++ ui/upgrade_options.gd
@@ -1,17 +1,5 @@
 extends CanvasLayer
 
-enum UpgradeOption {
-	INCREASE_HEALTH,
-	INCREASE_MOVEMENT_SPEED,
-	INCREASE_AMOUNT,
-	INCREASE_DAMAGE,
-	INCREASE_EXPERIENCE,
-	REDUCE_COOLDOWN,
-	INCREASE_SHOT_SPEED,
-	INCREASE_SHOT_LIFETIME,
-	REDUCE_SPREAD
-}
-
 
 func show_options():
 	randomize_options()
@@ -29,36 +17,16 @@ func hide_options():
 
 
 func randomize_options():
-	var shuffled_options = UpgradeOption.keys()
-	shuffled_options.shuffle()
-	var options = shuffled_options.slice(0, 3)
+	var options = Globals.get_random_upgrades()
 
-	for option in options:
+	for option_key in options:
+		var option = Globals.UpgradeOption[option_key]
 		var button = Button.new()
-		button.text = option
-		button.pressed.connect(func(): _button_pressed(UpgradeOption[option]))
+		button.text = option_key
+		button.pressed.connect(func(): _button_pressed(option))
 		$VBoxContainer/HBoxContainer.add_child(button)
 
 
-func _button_pressed(type: UpgradeOption):
-	match type:
-		UpgradeOption.INCREASE_HEALTH:
-			PlayerStats.max_health += PlayerStats.BASE_MAX_HEALTH * 0.1
-		UpgradeOption.INCREASE_MOVEMENT_SPEED:
-			PlayerStats.movement_speed += PlayerStats.BASE_MOVEMENT_SPEED * 0.1
-		UpgradeOption.INCREASE_EXPERIENCE:
-			PlayerStats.experience_multiplier += PlayerStats.BASE_EXPERIENCE_MULTIPLIER * 0.1
-		UpgradeOption.INCREASE_AMOUNT:
-			PlayerStats.shot_amount += 1
-		UpgradeOption.INCREASE_DAMAGE:
-			PlayerStats.shot_damage += 1
-		UpgradeOption.REDUCE_COOLDOWN:
-			PlayerStats.shot_cooldown -= PlayerStats.BASE_SHOT_COOLDOWN * 0.1
-		UpgradeOption.INCREASE_SHOT_SPEED:
-			PlayerStats.shot_speed += PlayerStats.BASE_SHOT_SPEED * 0.1
-		UpgradeOption.INCREASE_SHOT_LIFETIME:
-			PlayerStats.shot_lifetime += PlayerStats.BASE_SHOT_LIFETIME * 0.1
-		UpgradeOption.REDUCE_SPREAD:
-			PlayerStats.shot_spread -= PlayerStats.BASE_SHOT_SPREAD * 0.05
-
+func _button_pressed(option: Globals.UpgradeOption):
+	Globals.upgrade_stat(option)
 	hide_options()
